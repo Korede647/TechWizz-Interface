377900eaa430c27a212c4d9791be3072ba087326
Korede Anunusor <koredeanunusor@gmail.com>
2025-09-03 01:09:21 +0100

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import AboutUs from "./Pages/About/AboutUs";
 import ContactUs from "./Pages/Contact/ContactUs";
-import UserDashboard from "./User Dashboard/UserDashboard";
-import Gallery from "./Pages/Gallery/Gallery";
-import Login from "./Pages/login/Login";
-import Signup from "./Pages/signup/Signup";
 
 const App: React.FC = () => {
   return (
@@ -17,7 +13,6 @@ const App: React.FC = () => {
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/" element={<Homepage />} />
-        < Route path="/gallery" element={<Gallery />} />
       </Routes>
     </Router>
   );
